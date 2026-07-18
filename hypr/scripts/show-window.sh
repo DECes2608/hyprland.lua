a324756d9aabf7f6a8cbb7e39c6451c4eb1a646c
Deces <mrcesking@gmail.com>
2026-07-18 18:03:57 +0300

    Delete hypr directory

--- hypr/scripts/show-window.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CLASS=$(hyprctl activewindow -j | jq -r '.class')
-TITLE=$(hyprctl activewindow -j | jq -r '.title')
-sleep 0.1
-notify-send "Active Window" "$(printf 'class: %s\ntitle: %s' "$CLASS" "$TITLE")"
